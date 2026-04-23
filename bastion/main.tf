3174a7ae15ad3f2688ea644f44a0c6511c5e4b45
rajesh <mrajesh0525@gmail.com>
2026-04-24 02:12:51 +0530

    Adding Infra Code in a Seperate folder

--- bastion/main.tf
+++ bastion/main.tf
@@ -1,7 +1,11 @@
 resource "aws_instance" "bastion" {
     ami = locals.ami_id
     instance_type = t3.micro
-    vpc_security_group_ids = local.bastion_sg_id
+    vpc_security_group_ids = [local.bastion_sg_id]
     subnet_id = local.public_subnet_ids
+
+    tags = merge(local.common_tags, {
+        Name = "${var.project}-${var.environment}-bastion"
+    })
     
 }
